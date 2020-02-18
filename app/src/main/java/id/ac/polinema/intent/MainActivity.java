2c447419e9c78cb753afb21c120dcebde8ecf980
andyherdiansyah <aherdiansyah99@gmail.com>
2020-02-18 18:59:37 +0700

    Intent

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -1,6 +1,8 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void handleExplicitIntent (View view) {
+        Intent iten = new Intent(MainActivity.this, ExplicitIntentActivity.class);
+        startActivity(iten);
+    }
+    public void handleExit (View view) {
+        finish();
+    }
 
+    public void handleImplicitIntent(View view) {
+        Intent iten = new Intent (MainActivity.this, ImplicitIntentActivity.class);
+        startActivity(iten);
+    }
+    public void handleAndroidBundle(View view){
+        Intent iten = new Intent (MainActivity.this, BundleActivity.class);
+        startActivity(iten);
+    }
+    public void handleParcelable (View view) {
+        Intent iten = new Intent(MainActivity.this, ParcelableActivity.class);
+        startActivity(iten);
+    }
 }
